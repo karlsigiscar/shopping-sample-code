71fc84b3386bc5b83be44b8e1c9e885e1accc8ea
karlsigiscar <karlsigiscar@icloud.com>
2026-01-15 19:36:05 +0000

    JSON version where we download data in a service using Combine

--- ECommerceSampleApp/ECommerceSampleApp/ContentViewModel.swift
+++ ECommerceSampleApp/ECommerceSampleApp/ContentViewModel.swift
@@ -8,30 +8,36 @@
 import Combine
 import Foundation
 
-/// Thread yielding used here.
+/// Thread yielding
 /// Since properties marked with the @Published property wrapper will be accessed by SwiftUI,
-/// we mark ContentViewModel with @MainActor to ensure it's accessed from the main queue
-/// Any async call in the view model will be executed on a background queue
+/// we mark ContentViewModel with @MainActor to ensure they are always accessed from the main queue.
+/// By contrast, any async call in the view model will be executed on a background queue.
 @MainActor
 class ContentViewModel: NSObject, ObservableObject {
     
     @Published public var products = [ProductModel]()
-
-    enum AppError: Error {
-        case configuration
-    }
+    @Published public var isLoading: Bool = false
+    
+    private var productsService = ProductService()
+    private var cancellables: Set<AnyCancellable> = []
     
     override init() {
+        super.init()
+        subscribe()
         do {
-            let configuration = try Configuration()
-            guard let products = configuration.products else {
-                throw AppError.configuration
-            }
-            self.products = products
+            try productsService.loadProducts()
+            isLoading = true
         }
         catch {
             // Show user facing initialization error
             print(error)
         }
+    }
+    
+    private func subscribe() {
+        productsService.$products.dropFirst().receive(on: DispatchQueue.main).sink { [weak self] products in
+            self?.products = products
+            self?.isLoading = false
+        }.store(in: &cancellables)
     }
 }
---
+++ ECommerceSampleApp/ECommerceSampleApp/ProductService.swift
@@ -0,0 +1,42 @@
+//
+//  ProductService.swift
+//  ECommerceSampleApp
+//
+//  Created by Writer on 15/01/2026.
+//
+
+import Foundation
+import Combine
+
+class ProductService: NSObject, ObservableObject {
+    
+    enum AppError: Error {
+        case configuration
+    }
+    
+    @Published var products: [ProductModel] = []
+    private var cancellables: Set<AnyCancellable> = []
+    
+    public func loadProducts() throws {
+        // This could be deserialized directly from the included data.json file, but instead, we download it from github for the sake of example
+        guard let url = URL(string: "https://raw.githubusercontent.com/karlsigiscar/shopping-sample-code/refs/heads/main/ECommerceSampleApp/data.json") else{
+            return
+        }
+        
+        URLSession.shared.dataTaskPublisher(for: url)
+            .map(\.data)
+            .decode(type: Configuration.self, decoder: JSONDecoder())
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { completion in
+                if case let .failure(error) = completion {
+                    print("Api Error: \(error)")
+                }
+            },
+                  receiveValue: {[weak self] configuration in
+                guard let products = configuration.products else {
+                    return
+                }
+                self?.products = products
+            }).store(in: &cancellables)
+    }
+}
