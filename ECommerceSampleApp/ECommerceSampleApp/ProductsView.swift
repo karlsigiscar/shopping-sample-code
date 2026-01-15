da6b0dff9ba7af541d9f86b86fb1c1f65096d0c2
karlsigiscar <karlsigiscar@icloud.com>
2026-01-15 16:25:07 +0000

    Removed hover effect

--- ECommerceSampleApp/ECommerceSampleApp/ProductsView.swift
+++ ECommerceSampleApp/ECommerceSampleApp/ProductsView.swift
@@ -29,7 +29,6 @@ struct ProductsView: View {
                     }
                     .buttonStyle(.borderless)
                     .focusable()
-                    .hoverEffect(.lift)
                 }
             }
         }
